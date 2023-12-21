d46a53802b361f8cd9ae0ced356ab58fde0f5d73
TechGriffo <griffotech254@gmail.com>
2023-12-20 22:39:10 -0800

    3-main.py

---
+++ 0x06-python-classes/3-main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+Square = __import__('3-square').Square
+
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
+
+guillaume@ubuntu:~/0x06$ ./3-main.py
+Area: 9
+'Square' object has no attribute 'size'
+'Square' object has no attribute '__size'
+Area: 25
