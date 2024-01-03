f753d1ae4e38910f7a3688647c2ff7c167241fea
TechGriffo <griffotech254@gmail.com>
2024-01-03 04:18:05 -0800

    .

---
+++ 0x07-python-test_driven_development/2-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(matrix_divided(matrix, 3))
+print(matrix)
+
+guillaume@ubuntu:~/0x07$ ./2-main.py
+[[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
+[[1, 2, 3], [4, 5, 6]]
