240a93791867ff1971cea1aedfd4690d0b813603
TechGriffo <griffotech254@gmail.com>
2024-01-03 04:14:27 -0800

    .

---
+++ 0x07-python-test_driven_development/0-main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+add_integer = __import__('0-add_integer').add_integer
+
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
+
+guillaume@ubuntu:~/0x07$ ./0-main.py
+3
+98
+100
+98
+b must be an integer
+a must be an integer
