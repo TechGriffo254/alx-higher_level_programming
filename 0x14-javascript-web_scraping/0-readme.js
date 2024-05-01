530ce5df1e2c1c06f48728961ccc9555213b78e5
TechGriffo <griffotech254@gmail.com>
2024-04-30 17:51:24 -0700

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+
+#!/usr/bin/node
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
+
